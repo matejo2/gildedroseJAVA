0367323ea4d520ef18d99f64eea524b840ef25fb
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2019-08-01 22:59:28 +0200

    added qualitytest class

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,7 +3,6 @@ package com.gildedrose;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
@@ -14,50 +13,16 @@ public class GildedRoseTest {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertThat(app.items[0].name).isEqualTo("foo");
     }
 
-    @Test
-    public void QualityNeverDropsBelow_0() {
-        Item[] items = new Item[]{
-                new Item(itemValue.potion, 0, 0),
-                new Item(itemValue.elixir_of_mongoose, 2, 1)};
-        GildedRose app  = new GildedRose(items);
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        assertThat(items[0].quality).isEqualTo(0);
-        assertThat(items[1].quality).isEqualTo(0);
-    }
 
     @Test
-    public void QualityDropsDoulbeWhenSellInPasses() {
-        Item[] items = new Item[]{
-                new Item(itemValue.potion, 0, 30),
-                new Item(itemValue.elixir_of_mongoose, 0, 20)};
-        GildedRose app  = new GildedRose(items);
+    public void ItemExists2() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
         app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        System.out.println(items[0].toString() + "\n" + items[1].toString());
-        assertThat(items[0].quality).isEqualTo(22);
-        assertThat(items[1].quality).isEqualTo(12);
     }
 
-    @Test
-    public void QualityDropsByOne() {
-        Item[] items = new Item[]{
-                new Item(itemValue.potion, 12, 30),
-                new Item(itemValue.elixir_of_mongoose, 12, 20)};
-        GildedRose app  = new GildedRose(items);
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        System.out.println(items[0].toString() + "\n" + items[1].toString());
-        assertThat(items[0].quality).isEqualTo(26);
-        assertThat(items[1].quality).isEqualTo(16);
-    }
 }
 
---
+++ src/test/java/com/gildedrose/QualityTest.java
@@ -0,0 +1,134 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QualityTest {
+
+    ItemValue itemValue = new ItemValue();
+
+    @Test
+    public void QualityNeverDropsBelow_0() {
+        Item[] items = new Item[]{
+                new Item(itemValue.potion, 0, 0),
+                new Item(itemValue.elixir_of_mongoose, 2, 1)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        assertThat(items[0].quality).isEqualTo(0);
+        assertThat(items[1].quality).isEqualTo(0);
+    }
+
+    @Test
+    public void QualityDropsByOne() {
+        Item[] items = new Item[]{
+                new Item(itemValue.potion, 12, 30),
+                new Item(itemValue.elixir_of_mongoose, 12, 20)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString() + "\n" + items[1].toString());
+        assertThat(items[0].quality).isEqualTo(26);
+        assertThat(items[1].quality).isEqualTo(16);
+    }
+
+    @Test
+    public void QualityDropsDoulbeWhenSellInPasses() {
+        Item[] items = new Item[]{
+                new Item(itemValue.potion, 0, 30),
+                new Item(itemValue.elixir_of_mongoose, 0, 20)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString() + "\n" + items[1].toString());
+        assertThat(items[0].quality).isEqualTo(22);
+        assertThat(items[1].quality).isEqualTo(12);
+    }
+
+    @Test
+    public void QualityIsNeverOver_50() {
+        Item[] items = new Item[]{
+                new Item(itemValue.aged_brie, 9, 49)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(50);
+    }
+
+
+    @Test
+    public void SulfurasQualityIsAlways_80() {
+        Item[] items = new Item[]{
+                new Item(itemValue.sulfuras_hand_of_ragnaros, 9, 80)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(80);
+    }
+
+
+    @Test
+    public void AgedBrieQualityIncreases() {
+        Item[] items = new Item[]{
+                new Item(itemValue.aged_brie, 12, 0)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(2);
+    }
+
+    @Test
+    public void BackstagePassesQualityIncreasesWhenSellInOver_10() {
+        Item[] items = new Item[]{
+                new Item(itemValue.backstage_passes_to_a_TAFKAL80ETC_concert, 12, 0)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(2);
+    }
+
+    @Test
+    public void BackstagePassesQualityIncreasesBy_2_WhenSellInBelow_10() {
+        Item[] items = new Item[]{
+                new Item(itemValue.backstage_passes_to_a_TAFKAL80ETC_concert, 9, 0)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(4);
+    }
+
+    @Test
+    public void BackstagePassesQualityIncreasesBy_3_WhenSellInBelow_5() {
+        Item[] items = new Item[]{
+                new Item(itemValue.backstage_passes_to_a_TAFKAL80ETC_concert, 4, 0)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(6);
+    }
+
+    @Test
+    public void BackstagePassesQualityDropsTo_0_WhenSellInBelow_0() {
+        Item[] items = new Item[]{
+                new Item(itemValue.backstage_passes_to_a_TAFKAL80ETC_concert, 0, 23)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString());
+        assertThat(items[0].quality).isEqualTo(0);
+    }
+}
