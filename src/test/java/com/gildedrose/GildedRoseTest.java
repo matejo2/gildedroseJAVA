c2c9fca11593b8648eaa020ec4e8493fad65cf42
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2019-08-01 22:20:45 +0200

    testing quality dropping of items

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -29,5 +29,35 @@ public class GildedRoseTest {
         assertThat(items[0].quality).isEqualTo(0);
         assertThat(items[1].quality).isEqualTo(0);
     }
+
+    @Test
+    public void QualityDropsDoulbeWhenSellInPasses() {
+        Item[] items = new Item[]{
+                new Item(itemValue.potion, 0, 30),
+                new Item(itemValue.elixir_of_mongoose, 0, 20)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString() + "\n" + items[1].toString());
+        assertThat(items[0].quality).isEqualTo(22);
+        assertThat(items[1].quality).isEqualTo(12);
+    }
+
+    @Test
+    public void QualityDropsByOne() {
+        Item[] items = new Item[]{
+                new Item(itemValue.potion, 12, 30),
+                new Item(itemValue.elixir_of_mongoose, 12, 20)};
+        GildedRose app  = new GildedRose(items);
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        app.updateQuality();
+        System.out.println(items[0].toString() + "\n" + items[1].toString());
+        assertThat(items[0].quality).isEqualTo(26);
+        assertThat(items[1].quality).isEqualTo(16);
+    }
 }
 
