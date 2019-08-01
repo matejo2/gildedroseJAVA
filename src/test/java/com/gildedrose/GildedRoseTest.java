81dfc1ff5caf0bb5deb184137d31e8c235cddc8e
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2019-08-01 21:57:27 +0200

    refactored some tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,95 +1,18 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
-    private String actualOutput;
-
-    @Before
-    public void setUp() throws Exception {
-        StringBuilder stringBuilder = new StringBuilder();
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
-
-        GildedRose app = new GildedRose(items);
-
-        int days = 3;
-        int startDay = 0;
-        if (startDay > 0) {
-            days = startDay + 1;
-        }
-
-        for (int i = 0; i < days; i++) {
-            stringBuilder.append("-------- day " + i + " --------");
-            stringBuilder.append("\n");
-            stringBuilder.append("name, sellIn, quality");
-            stringBuilder.append("\n");
-            for (Item item : items) {
-                stringBuilder.append(item + "\n");
-            }
-            app.updateQuality();
-        }
-        actualOutput = stringBuilder.toString();
-    }
-
     @Test
     public void ItemExists() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-
-    @Test
-    public void ActualOutputIsRight() {
-        assertThat(actualOutput).isEqualTo(expectedOutput);
-    }
-
-    private String expectedOutput = "-------- day 0 --------\n" +
-            "name, sellIn, quality\n" +
-            "+5 Dexterity Vest, 10, 20\n" +
-            "Aged Brie, 2, 0\n" +
-            "Elixir of the Mongoose, 5, 7\n" +
-            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
-            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
-            "Conjured Mana Cake, 3, 6\n" +
-            "-------- day 1 --------\n" +
-            "name, sellIn, quality\n" +
-            "+5 Dexterity Vest, 9, 19\n" +
-            "Aged Brie, 1, 1\n" +
-            "Elixir of the Mongoose, 4, 6\n" +
-            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
-            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
-            "Conjured Mana Cake, 2, 5\n" +
-            "-------- day 2 --------\n" +
-            "name, sellIn, quality\n" +
-            "+5 Dexterity Vest, 8, 18\n" +
-            "Aged Brie, 0, 2\n" +
-            "Elixir of the Mongoose, 3, 5\n" +
-            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
-            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 13, 22\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 8, 50\n" +
-            "Backstage passes to a TAFKAL80ETC concert, 3, 50\n" +
-            "Conjured Mana Cake, 1, 4\n";
 }
+
--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,10 +1,18 @@
 package com.gildedrose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.junit.Before;
+import org.junit.Test;
 
-        Item[] items = new Item[] {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TexttestFixture {
+
+    private String actualOutput;
+
+    @Before
+    public void setUp() throws Exception {
+        StringBuilder stringBuilder = new StringBuilder();
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -14,24 +22,67 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
+        int days = 3;
+        int startDay = 0;
+        if (startDay > 0) {
+            days = startDay + 1;
         }
 
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            stringBuilder.append("-------- day " + i + " --------");
+            stringBuilder.append("\n");
+            stringBuilder.append("name, sellIn, quality");
+            stringBuilder.append("\n");
             for (Item item : items) {
-                System.out.println(item);
+                stringBuilder.append(item + "\n");
             }
-            System.out.println();
             app.updateQuality();
         }
+        actualOutput = stringBuilder.toString();
     }
 
+    @Test
+    public void ActualOutputIsRight() {
+        assertThat(actualOutput).isEqualTo(expectedOutput);
+    }
+
+    private String expectedOutput = "-------- day 0 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 10, 20\n" +
+            "Aged Brie, 2, 0\n" +
+            "Elixir of the Mongoose, 5, 7\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+            "Conjured Mana Cake, 3, 6\n" +
+            "-------- day 1 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 9, 19\n" +
+            "Aged Brie, 1, 1\n" +
+            "Elixir of the Mongoose, 4, 6\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
+            "Conjured Mana Cake, 2, 5\n" +
+            "-------- day 2 --------\n" +
+            "name, sellIn, quality\n" +
+            "+5 Dexterity Vest, 8, 18\n" +
+            "Aged Brie, 0, 2\n" +
+            "Elixir of the Mongoose, 3, 5\n" +
+            "Sulfuras, Hand of Ragnaros, 0, 80\n" +
+            "Sulfuras, Hand of Ragnaros, -1, 80\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 13, 22\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 8, 50\n" +
+            "Backstage passes to a TAFKAL80ETC concert, 3, 50\n" +
+            "Conjured Mana Cake, 1, 4\n";
 }
+
+
